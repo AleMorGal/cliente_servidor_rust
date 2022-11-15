72b1ee7ffecd49676363c929474cf410323089f6
cardcathouse <mebr1200@gmail.com>
2022-11-14 23:56:16 -0600

    fixed main, add sql file, changed .env

---
+++ BD/createDB.sql
@@ -0,0 +1,3 @@
+create database udp_server_client;
+use udp_server_client;
+create table files(ID int(11) NOT NULL, fileName varchar(255) NOT NULL, filePath varchar(255) NOT NULL, extension varchar(15) NOT NULL);
--- BD/src/main.rs
+++ BD/src/main.rs
@@ -24,31 +24,10 @@ struct file_p{
 async fn main() -> Result<(), sqlx::Error>
 {
     //Vector en el que se guarda cada entrada de la base de datos
-    //let mut x: Vec<File> = Vec::new();
     let pool = MySqlPoolOptions::new()
         .max_connections(2)
         .connect("mysql://root:root@localhost/udp_test")
         .await?;
-    /*sqlx::query("INSERT INTO files VALUES (?,?,?)")
-        .bind(8)
-        .bind("archivo10")
-        .bind("/run/media/cardcathouse/Main/Documentos/SistemasDistribuidos/Prácticas/")
-        .execute(&pool)
-        .await?;*/
-    
-    /*Pasos siguientes:
-        -Guardar ID y nombre de archivo en arreglo/vector 
-        -Se muestra al usuario lista de archivos que hay disponibles
-        -Usuario inserta ID de archivo que quiere consultar
-        -Cliente manda ID de archivo a servidor
-        -Servidor obtiene ruta de archivo a partir de ID
-        -Servidor obtiene archivo y manda a cliente a través de UDP
-        -Fin*/
-    
-    /*Guardando lista de archivos en struct. 
-    ¿Arreglo de structs para mostrar todos? 
-    ¿Comprobar que ID existe en lista?
-    */
 
     /*Mostrando entradas en base de datos
     Para usar macro query_as! se requiere:
@@ -63,8 +42,7 @@ async fn main() -> Result<(), sqlx::Error>
     */
 
     /*Todas las entradas se guardan en un vector que guarda el struct File
-    File contiene ID de archivo y su nombre
-    ¿cómo hacer para que solo se imprima datos sin some? */
+    File contiene ID de archivo y su nombre*/
     println!("Mostrando archivos disponibles:");
     let files: Vec<Archivo> = 
         sqlx::query_as!(Archivo, r"select id, filename from files")
@@ -78,14 +56,13 @@ async fn main() -> Result<(), sqlx::Error>
     let b1 = std::io::stdin().read_line(&mut id_select).unwrap();
 
     //Pasando ID de documento a BD, debería regresar ruta de documento
-    //ruta se tiene que guardar en un struct...creo.
     println!("Recuperando documento...");
     let f_path = sqlx::query_as!(file_p,
         "select fileName, filePath, extension from files where id = ?", id_select)
         .fetch_one(&pool)
         .await?;
     
-    println!("Ruta del archivo: {}", f_path.filePath);
+    //println!("Ruta del archivo: {}{}", f_path.filePath, f_path.extension);
 
     /*Una vez que tengamos ruta, abrimos documento y se convierte a bytes, que
     se almacenan como Vec<u8>. Ese vector de datos binarios es el que se envía
@@ -98,14 +75,18 @@ async fn main() -> Result<(), sqlx::Error>
     //Pedimos a usuario insertar nueva ubicación donde se va a guardar archivo
     println!("Inserta la ruta en donde se guardará el archivo:");
     let b2 = std::io::stdin().read_line(&mut new_path).unwrap();
+    //Se le quita el newline a la ruta
+    new_path.pop();
     //Ahora que ya tenemos nueva ruta, lo juntamos con nombre de archivo y extensión
-    let mut new_path = String::new();
-    new_path.push_str(&f_path.fileName);
-    new_path.push_str(&f_path.extension);
+    //Por ahora no copia la extensión. Intentar corregir.
+    let mut v = vec![new_path,"/".to_string(),f_path.fileName,f_path.extension];
+    //println!("{:#?}", v);
+    let s: String = v.concat();
+    //println!("{}", s);
     //Creamos el archivo en el disco del cliente
-    let mut fw = File::create(new_path).expect("No se puede crear archivo");
+    let mut fw = File::create(s).expect("No se puede crear archivo");
     //Guardamos el vector de bytes en el archivo
     fw.write_all(&data).expect("No se puede crear archivo");
-
+    println!("El archivo se guardó exitosamente");
     Ok(())
 }
